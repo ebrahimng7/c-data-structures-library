f979c65943335d24f30327cc95c2e8c815d1461b
ebrahimng7 <ebrahimnouri79@gmail.com>
2026-06-08 13:00:50 +0330

    Implement vector push_back and dynamic resizing

--- src/vector.c
+++ src/vector.c
@@ -1,7 +1,12 @@
 #include <stdlib.h>
-#include "/home/ebrahim/Documents/C Projects/c-data-structure-library/include/vector.h"
+#include "vector.h"
 
 #define INITIAL_CAPACITY 4
+
+static int vector_resize(
+    Vector* vec,
+    size_t new_capacity
+);
 
 Vector* vector_create(void)
 {
@@ -35,4 +40,50 @@ void vector_destroy(Vector* vec)
 
     free(vec->data);
     free(vec);
+}
+
+static int vector_resize(
+    Vector* vec,
+    size_t new_capacity
+)
+{
+    int* new_data = realloc(
+        vec->data,
+        new_capacity * sizeof(int)
+    );
+
+    if (new_data == NULL)
+    {
+        return 0;
+    }
+
+    vec->data = new_data;
+    vec->capacity = new_capacity;
+
+    return 1;
+}
+
+void vector_push_back(
+    Vector* vec,
+    int value
+)
+{
+    if (vec == NULL)
+    {
+        return;
+    }
+
+    if (vec->size == vec->capacity)
+    {
+        if (!vector_resize(
+                vec,
+                vec->capacity * 2))
+        {
+            return;
+        }
+    }
+
+    vec->data[vec->size] = value;
+
+    vec->size++;
 }
