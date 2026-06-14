303562f9c03099e84f630547432f6f1b1d5e835c
ebrahimng7 <ebrahimnouri79@gmail.com>
2026-06-14 21:54:54 +0330

    Add tests for linked list front and back operations

--- tests/test_linked_list.c
+++ tests/test_linked_list.c
@@ -84,11 +84,30 @@ void test_linked_list_push_back(void)
     printf("test_linked_list_push_back PASSED\n");
 }
 
+void test_linked_list_front_back(void)
+{
+    LinkedList* list = linked_list_create();
+
+    linked_list_push_back(list, 10);
+    linked_list_push_back(list, 20);
+    linked_list_push_back(list, 30);
+
+    assert(linked_list_front(list) == 10);
+
+    assert(linked_list_back(list) == 30);
+
+    linked_list_destroy(list);
+
+    printf("test_linked_list_front_back PASSED\n");
+}
+
 int main(void)
 {
     test_linked_list_create_destroy();
     test_linked_list_push_front();
     test_linked_list_push_back();
+    test_linked_list_front_back();
+
     printf("\nAll linked list tests passed successfully!\n");
 
     return 0;
