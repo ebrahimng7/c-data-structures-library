97a925823d3c9c44f6dd166e8e205f95e1cbe853
ebrahimng7 <ebrahimnouri79@gmail.com>
2026-06-14 21:51:08 +0330

    Add tests for linked list push back operation

--- tests/test_linked_list.c
+++ tests/test_linked_list.c
@@ -51,11 +51,44 @@ void test_linked_list_push_front(void)
     printf("test_linked_list_push_front PASSED\n");
 }
 
+void test_linked_list_push_back(void)
+{
+    LinkedList* list = linked_list_create();
+
+    assert(linked_list_push_back(list, 10) == 0);
+
+    assert(list->size == 1);
+
+    assert(list->head == list->tail);
+
+    assert(list->head->data == 10);
+
+    assert(linked_list_push_back(list, 20) == 0);
+
+    assert(list->size == 2);
+
+    assert(list->head->data == 10);
+
+    assert(list->tail->data == 20);
+
+    assert(list->head->next == list->tail);
+
+    assert(linked_list_push_back(list, 30) == 0);
+
+    assert(list->size == 3);
+
+    assert(list->tail->data == 30);
+
+    linked_list_destroy(list);
+
+    printf("test_linked_list_push_back PASSED\n");
+}
+
 int main(void)
 {
     test_linked_list_create_destroy();
     test_linked_list_push_front();
-    
+    test_linked_list_push_back();
     printf("\nAll linked list tests passed successfully!\n");
 
     return 0;
